c4edc4ef0a75a2b711784a0c7b22adb00159cb78
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-24 14:40:18 +0200

    start

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="./styles//style.css">
+
+    <title> PHP Google FAQ</title>
+</head>
+
+<body>
+    <header>
+        <!-- HEADER DELLA PAGINA CON IL MENU E IL PROFILO -->
+    </header>
+
+    <main>
+        <!-- CORPO DELLA PAGINA CON LE DOMANDE E LE RISPOTE -->
+    </main>
+
+    <footer>
+        <!-- FOOTER -->
+    </footer>
+</body>
+</html>
