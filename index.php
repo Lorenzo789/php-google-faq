3f067523e148334ffd821bc71f3c3dcc911677a7
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-08-24 16:50:09 +0200

    header done

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     <!-- MY CSS -->
     <link rel="stylesheet" href="./styles//style.css">
 
@@ -13,6 +17,24 @@
 <body>
     <header>
         <!-- HEADER DELLA PAGINA CON IL MENU E IL PROFILO -->
+        <div class="flexbox">
+            <div>
+                <h2>Google Privacy e termini</h2>
+            </div>
+            <div>
+                <i class="menu-grid fa-solid fa-table-cells"></i>
+                <i class="red-circle fa-solid fa-circle"></i>
+            </div>
+        </div>
+        <div>
+            <ul class="container-ul">
+                <li><a href="#">Introduzione</a></li>
+                <li><a href="#">Norme sulla privacy</a></li>
+                <li><a href="#">Termini di servizio</a></li>
+                <li><a href="#">Tecnologie</a></li>
+                <li><a class="active" href="#">Domande frequenti</a></li>
+            </ul>
+        </div>
     </header>
 
     <main class="centered">
